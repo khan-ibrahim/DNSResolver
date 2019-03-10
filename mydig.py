f9f281df30aaa0bec5d76f59759440701d79e551
Ibrahim Khan <5855449+khan-ibrahim@users.noreply.github.com>
2019-03-10 18:06:50 -0400

    implemented basic one round resolution

--- mydig.py
+++ mydig.py
@@ -1,11 +1,12 @@
 import sys
 import dns.query
 import dns.message
+import re
 
 __author__ = "Ibrahim Khan"
 
 
-rootServers['198.41.0.4', '199.9.14.201', '192.33.4.12', '199.7.91.13', \
+rootServers = ['198.41.0.4', '199.9.14.201', '192.33.4.12', '199.7.91.13', \
     '192.203.230.10', '192.5.5.241', '192.112.36.4', '198.97.190.53', \
     '192.36.148.17', '192.58.128.30', '193.0.14.129', '202.12.27.33']
 
@@ -13,8 +14,31 @@ rootServer = rootServers[6] #DOD NIC
 
 def main(domainStr):
     print("starting query for ", domainStr)
-    print(queryServer(domainStr, 'NS', rootServer))
-    print(queryServer(domainStr, 'A', rootServer))
+    recursiveNSResolver(domainStr, rootServer)
+
+
+def recursiveNSResolver(domainStr, server):
+    print('querying', server, 'for domain', domainStr)
+    r = queryServer(domainStr, 'A', server).__str__()
+
+    pattern = domainStr + r'\. \d+ IN A (\d+\.\d+\.\d+\.\d+)'
+    print('checking:', pattern)
+    match = re.search(pattern, r)
+    if match:
+        print('answer match found', match.group(1))
+        print(r)
+        exit()
+    else:
+        print('answer not yet found')
+
+    pattern = r'(\d+\.\d+\.\d+\.\d+)'
+    match = re.search(pattern, r)
+    if match:
+        print('match found!', match.group(1))
+        recursiveNSResolver(domainStr, match.group(1))
+    else:
+        print('No IP found -------')
+        print(r)
 
 def queryServer(qname, rdtype, serverAddr):
     q = dns.message.make_query(qname, rdtype)
